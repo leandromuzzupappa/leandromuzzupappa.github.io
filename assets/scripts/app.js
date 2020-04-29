60b97981c7548cc645c828c3e72d01fedf4b2ae7
Leandro Muzzupappa <leann.muzzu@gmail.com>
2020-04-29 02:15:41 -0300

    superPortfolio

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,198 +1,21 @@
-const portfolioBtn = $('#portfolioBtn');
-const Home = $('.Home');
-const Portfolio = $('.Portfolio > .container');
-const imagePath = '/assets/images/';
+const cargarProyectos = document.querySelector('#proyectos');
+const linkIcon = '<svg width="25" height="25" viewBox="0 0 25 25"><g id="Group_8" data-name="Group 8" transform="translate(-1411 -2870)"><rect id="Rectangle_20" data-name="Rectangle 20" class="cls-1" width="25" height="25"transform="translate(1411 2870)" /><path id="Icon_awesome-external-link-alt" data-name="Icon awesome-external-link-alt" class="cls-2"d="M18.506.741V4.694a.776.776,0,0,1-1.316.524l-1.147-1.1L8.218,11.636a.793.793,0,0,1-1.09,0l-.727-.7a.721.721,0,0,1,0-1.048l7.824-7.521-1.147-1.1A.739.739,0,0,1,13.623,0h4.112A.757.757,0,0,1,18.506.741ZM13.077,8.363l-.514.494a.727.727,0,0,0-.226.524v4.454H2.056V3.953h8.482a.787.787,0,0,0,.545-.217l.514-.494a.738.738,0,0,0-.545-1.265H1.542A1.513,1.513,0,0,0,0,3.459V14.33a1.513,1.513,0,0,0,1.542,1.482H12.851a1.513,1.513,0,0,0,1.542-1.482V8.887A.775.775,0,0,0,13.077,8.363Z"transform="translate(1414 2875)" /></g></svg>';
+const githubIcon = '<svg width="25" height="25" viewBox="0 0 25 25"><g id="Group_8" data-name="Group 8" transform="translate(-1412 -2870)"><rect id="Rectangle_20" data-name="Rectangle 20" class="cls-1" width="25" height="25"transform="translate(1412 2870)" /><path id="Icon_awesome-github" data-name="Icon awesome-github" class="cls-2"d="M5.96,14.153c0,.07-.083.126-.187.126s-.2-.045-.2-.126.083-.126.187-.126S5.96,14.073,5.96,14.153ZM4.842,14c-.025.07.047.15.154.171a.171.171,0,0,0,.223-.07c.022-.07-.047-.15-.154-.181A.187.187,0,0,0,4.842,14Zm1.588-.059c-.1.024-.176.091-.165.171s.1.115.212.091.176-.091.165-.161S6.534,13.926,6.43,13.937ZM8.794.563A8.495,8.495,0,0,0,0,9.078a8.772,8.772,0,0,0,6.089,8.348c.46.08.621-.2.621-.422S6.7,15.595,6.7,14.862c0,0-2.515.524-3.043-1.04,0,0-.41-1.016-1-1.277,0,0-.823-.548.057-.537a1.907,1.907,0,0,1,1.387.9,1.93,1.93,0,0,0,2.619.729A1.919,1.919,0,0,1,7.3,12.46c-2.008-.216-4.034-.5-4.034-3.857A2.607,2.607,0,0,1,4.11,6.548,3.21,3.21,0,0,1,4.2,4.178c.751-.227,2.479.942,2.479.942a8.726,8.726,0,0,1,4.512,0s1.728-1.173,2.479-.942a3.209,3.209,0,0,1,.093,2.37A2.679,2.679,0,0,1,14.692,8.6c0,3.368-2.116,3.637-4.124,3.857a2.033,2.033,0,0,1,.611,1.619c0,1.176-.011,2.632-.011,2.918,0,.227.165.5.621.422a8.689,8.689,0,0,0,6.028-8.341C17.817,4.238,13.776.563,8.794.563ZM3.492,12.6c-.047.035-.036.115.025.181s.14.08.187.035.036-.115-.025-.181S3.538,12.555,3.492,12.6ZM3.1,12.317c-.025.045.011.1.083.136a.109.109,0,0,0,.154-.024c.025-.045-.011-.1-.083-.136S3.129,12.282,3.1,12.317ZM4.268,13.56c-.057.045-.036.15.047.216s.187.091.233.035.025-.15-.047-.216S4.314,13.5,4.268,13.56Zm-.41-.513c-.057.035-.057.126,0,.206s.154.115.2.08a.156.156,0,0,0,0-.216C4.009,13.036,3.916,13,3.858,13.047Z"transform="translate(1416 2873.438)" /></g></svg>';
 
-portfolioBtn.click(function () {
-    if ( !Home.hasClass('portfolioAbierto') ) {
-        Home.addClass('portfolioAbierto');
-    } else {
-        Home.removeClass('portfolioAbierto');
-    }
+window.addEventListener('load', () => {
+
+    proyectos.map(proyecto => {
+        let web = proyecto.links.web != '' ? `<a href="${proyecto.links.web}" target="_blank">${linkIcon}</a>` : '';
+        let github = proyecto.links.github != '' ? `<a href="${proyecto.links.github}" target="_blank">${githubIcon}</a>` : '';
+
+        cargarProyectos.innerHTML += `
+        <tr>
+            <td>${proyecto.ano}</td>
+            <td>${proyecto.titulo}</td>
+            <td>${proyecto.lugar}</td>
+            <td>${proyecto.stack}</td>
+            <td>${ web + github }</td>
+        </tr>
+        `;
+    })
 })
-
-
-// Project template
-function createProject(project, previewUrl, faviconUrl, title, stack, web, github, bgcolor) {
-    if (project === undefined) {
-        throw new Error('Falta el nombre del proyecto');
-    }
-    if (previewUrl === undefined) {
-        throw new Error('Falta la preview del proyecto');
-    }
-    if (faviconUrl === undefined) {
-        throw new Error('Falta el favicon del proyecto');
-    }
-    if (title === undefined) {
-        throw new Error('Falta el titulo del proyecto');
-    }
-    if (stack === undefined) {
-        throw new Error('Falta el stack del proyecto');
-    }
-    if (web === undefined) {
-        throw new Error('Falta el link del proyecto');
-    }
-    if (github === undefined) {
-        throw new Error('Falta la repo de github del proyecto');
-    }
-
-
-    return `<div class="Trabajos" proyecto="${project}" style="background-color: ${bgcolor};">
-        <div class="preview">
-            <img src="${previewUrl}" alt="${project}">
-        </div>
-        <div class="favicon">
-            <img src="${faviconUrl}" alt="${project}">
-        </div>
-        <div class="bajada">
-            <h4>
-                ${title}
-            </h4>
-            <p>
-                ${stack}
-            </p>
-        </div>
-        <div class="linkcitos">
-            <a href="${web}" target="_blank">Website</a> - 
-            <a href="${github}" target="_blank">Github</a>
-        </div>
-    </div> `;
-}
-
-// Agregar proyecto al array
-var items = [
-    {
-        project: 'redhat-open-culture',
-        bgcolor: '#021E2C',
-        previewUrl: imagePath + 'p-redhat-oc.jpg',
-        faviconUrl: imagePath + 'f-redhat.png',
-        titulo: 'Red Hat Open Culture',
-        stack: 'HTML- Vanilla Js',
-        web: 'https://www.redhat.com/es/open-culture/home',
-        github: '#github'
-    },
-    {
-        project: 'delusarreta-abogados',
-        bgcolor: '#021E2C',
-        previewUrl: imagePath + 'p-delusarreta.png',
-        faviconUrl: imagePath + 'f-delusarreta.png',
-        titulo: 'DELUSARRETA ABOGADOS',
-        stack: 'HTML - CSS - Js',
-        web: 'http://delusarreta.com.ar/',
-        github: '#github'
-    },
-    {
-        project: 'Airpostcard',
-        bgcolor: '#000046',
-        previewUrl: imagePath + 'p-airpostcard.jpg',
-        faviconUrl: imagePath + 'f-airpostcard.png',
-        titulo: 'Airpostcard',
-        stack: 'Bubble Platform',
-        web: 'http://airpostcard.com/',
-        github: '#github'
-    },
-    {
-        project: 'EMAIL SUMMIT',
-        bgcolor: '#031427',
-        previewUrl: imagePath + 'p-emailsummit.png',
-        faviconUrl: imagePath + 'f-emailsummit.png',
-        titulo: 'EMAIL SUMMIT',
-        stack: 'HTML - SASS - jQuery',
-        web: 'http://emailsummit.org/',
-        github: '#github'
-    },
-    {
-        project: 'MOBILE SUMMIT',
-        bgcolor: '#05142C',
-        previewUrl: imagePath + 'p-mobilesummit.png',
-        faviconUrl: imagePath + 'f-mobilesummit.png',
-        titulo: 'MOBILE SUMMIT',
-        stack: 'HTML - SASS - jQuery',
-        web: 'http://www.mobilesummit.com.ar/',
-        github: '#github'
-    },
-    {
-        project: 'Makro Cajas Navideñas',
-        bgcolor: '#D51E24',
-        previewUrl: imagePath + 'p-makro-cajas.jpg',
-        faviconUrl: imagePath + 'f-makro-cajas.png',
-        titulo: 'Makro Cajas Navideñas',
-        stack: 'HTML - SASS - jQuery',
-        web: 'http://mobilesummit.com.ar/',
-        github: '#github'
-    },
-    {
-        project: 'VIDT CENTRO MEDICO',
-        bgcolor: '#AA2A3B',
-        previewUrl: imagePath + 'p-vidt.png',
-        faviconUrl: imagePath + 'f-vidt.png',
-        titulo: 'VIDT CENTRO MEDICO',
-        stack: 'PHP - LESS - jQuery',
-        web: 'http://www.vidt.com.ar/',
-        github: '#github'
-    },
-    {
-        project: 'MASSIVE AGENCY',
-        bgcolor: '#ED1B2F',
-        previewUrl: imagePath + 'p-massive.png',
-        faviconUrl: imagePath + 'f-massive.png',
-        titulo: 'MASSIVE AGENCY',
-        stack: 'PHP - SASS - Js',
-        web: 'https://massive.ag/',
-        github: '#github'
-    },
-    {
-        project: 'GLOBAL BOOSTERS',
-        bgcolor: '#EC3709',
-        previewUrl: imagePath + 'p-globalboosters.png',
-        faviconUrl: imagePath + 'f-globalboosters.png',
-        titulo: 'GLOBAL BOOSTERS',
-        stack: 'HTML - B4 - jQuery',
-        web: 'http://globalboosters.com/',
-        github: '#github'
-    },
-    {
-        project: 'RED HAT SMART START',
-        bgcolor: '#2E363D',
-        previewUrl: imagePath + 'p-redhat-ss.png',
-        faviconUrl: imagePath + 'f-redhat.png',
-        titulo: 'RED HAT SMART START',
-        stack: 'HTML - Vanilla Js',
-        web: 'https://www.redhat.com/es/campaign/smart-start-consulting',
-        github: '#github'
-    },
-    {
-        project: 'ESTUDIO ABOGADOS',
-        bgcolor: '#122E3C',
-        previewUrl: imagePath + 'p-sabatellacaffieri.png',
-        faviconUrl: imagePath + 'f-sabatellacaffieri.png',
-        titulo: 'ESTUDIO ABOGADOS',
-        stack: 'HTML - CSS',
-        web: 'http://www.scabogados.com.ar/',
-        github: '#github'
-    },
-    {
-        project: 'PERFORMANCE S.R.L',
-        bgcolor: '#1C549F',
-        previewUrl: imagePath + 'p-performance.png',
-        faviconUrl: imagePath + 'f-performance.png',
-        titulo: 'PERFORMANCE S.R.L',
-        stack: 'HTML - LESS - jQuery',
-        web: 'http://www.performancesrl.com.ar/',
-        github: '#github'
-    },
-]
-
-// Recorro el array y creo un elemento en el html
-for (let i = 0; i < items.length; i++) {
-    Portfolio.append(createProject(
-        items[i].project,
-        items[i].previewUrl,
-        items[i].faviconUrl,
-        items[i].titulo,
-        items[i].stack,
-        items[i].web,
-        items[i].github,
-        items[i].bgcolor
-    ));
-}
---
+++ assets/scripts/proyectos.js
@@ -0,0 +1,183 @@
+const proyectos = [{
+        ano: 2020,
+        titulo: 'Web de noticias - en desarrollo',
+        lugar: 'Delenio',
+        stack: 'React - Node - Express - MySQL - Diseño',
+        links: {
+            web: '',
+            github: 'https://github.com/leandromuzzupappa/generadores-tv'
+        }
+    },
+    {
+        ano: 2020,
+        titulo: 'Landing page informativa',
+        lugar: 'Delenio',
+        stack: 'HTML - SCSS - Javascript - PHPMailer - Diseño',
+        links: {
+            web: 'https://www.delenio.net/ventasremotas/',
+            github: 'https://github.com/leandromuzzupappa/delenio-ventasremotas'
+        }
+    },
+    {
+        ano: 2020,
+        titulo: 'Integrar un formulario HTML con Google Sheets',
+        lugar: '-',
+        stack: 'HTML - Javascript - Google Forms - Diseño',
+        links: {
+            web: 'https://leandromuzzupappa.github.io/supervielle-std/web/',
+            github: 'https://github.com/leandromuzzupappa/supervielle-std'
+        }
+    },
+    {
+        ano: 2020,
+        titulo: 'Formulario interactivo y sistema de tickets',
+        lugar: 'Tribu Creativa',
+        stack: 'PHP - Javascript - Diseño',
+        links: {
+            web: 'https://www.chocolatecolonial.com.ar/personalizar.php',
+            github: 'https://github.com/leandromuzzupappa/chocolate-colonial'
+        }
+    },
+    {
+        ano: 2020,
+        titulo: 'Produck',
+        lugar: '-',
+        stack: 'HTML - SCSS',
+        links: {
+            web: 'https://github.com/leandromuzzupappa/produck',
+            github: 'https://leandromuzzupappa.github.io/produck'
+        }
+    },
+    {
+        ano: 2019,
+        titulo: 'Website para canjear puntos ASOCIART',
+        lugar: 'Woonky',
+        stack: 'PHP - MySQL',
+        links: {
+            web: 'http://www.clubasociart.com.ar/devel/',
+            github: 'https://github.com/leandromuzzupappa/club-asociart'
+        }
+    },
+    {
+        ano: 2019,
+        titulo: 'Script para unir bases de datos',
+        lugar: 'Woonky',
+        stack: 'PHP - MySQL',
+        links: {
+            web: '',
+            github: 'https://github.com/leandromuzzupappa/sc-unir-databases'
+        }
+    },
+    {
+        ano: 2019,
+        titulo: 'Script para editar flyers online y descargalo',
+        lugar: 'Woonky',
+        stack: 'PHP - MySQL',
+        links: {
+            web: '',
+            github: 'https://github.com/leandromuzzupappa/flyers'
+        }
+    },
+    {
+        ano: 2019,
+        titulo: 'Expo Real State 2019',
+        lugar: 'Global Dardos',
+        stack: 'HTML - SCSS - Javascript',
+        links: {
+            web: 'http://www.exporealestatemexico.com/2019/',
+            github: 'https://github.com/leandromuzzupappa/expo-real-state'
+        }
+    },
+    {
+        ano: 2018,
+        titulo: 'Landing Mobile Summit',
+        lugar: 'Global Dardos',
+        stack: 'HTML - SCSS - Javascript',
+        links: {
+            web: 'http://www.mobilesummit.com.ar/2018/',
+            github: ''
+        }
+    },
+    {
+        ano: 2018,
+        titulo: 'Landing Email Summit',
+        lugar: 'Global Dardos',
+        stack: 'HTML - SCSS - Javascript',
+        links: {
+            web: 'http://emailsummit.org/2018/',
+            github: 'https://github.com/leandromuzzupappa/email-summit-2018'
+        }
+    },
+    {
+        ano: 2018,
+        titulo: 'Red Hat Open Culture - landing pages',
+        lugar: 'Global Dardos',
+        stack: 'HTML - SCSS - Javascript',
+        links: {
+            web: 'https://www.redhat.com/es/open-culture/home',
+            github: 'https://github.com/leandromuzzupappa/RedHat-Open-Culture'
+        }
+    },
+    {
+        ano: 2018,
+        titulo: 'Global Boosters',
+        lugar: '-',
+        stack: 'HTML - bootstrap - Javascript',
+        links: {
+            web: 'http://globalboosters.com/es-Es/index.html',
+            github: 'https://github.com/leandromuzzupappa/global-boosters'
+        }
+    },
+    {
+        ano: 2018,
+        titulo: 'Website para Performance S.R.L.',
+        lugar: 'MASSIVE',
+        stack: 'PHP - SCSS',
+        links: {
+            web: 'http://www.performancesrl.com.ar/',
+            github: ''
+        }
+    },
+    {
+        ano: 2018,
+        titulo: 'Website y seccion de noticias para centro medico',
+        lugar: 'MASSIVE',
+        stack: 'PHP - Bootstrap - PHPMailer',
+        links: {
+            web: 'https://www.vidt.com.ar/',
+            github: 'https://github.com/leandromuzzupappa/VIDT-Centro-Medico'
+        }
+    },
+    {
+        ano: 2018,
+        titulo: 'Website MASSIVE AG',
+        lugar: 'MASSIVE',
+        stack: 'HTML - SCSS - Javascript',
+        links: {
+            web: 'https://massive.ag/',
+            github: ''
+        }
+    },
+    {
+        ano: 2017,
+        titulo: 'Website para un centro de natacion',
+        lugar: '-',
+        stack: 'Wordpress',
+        links: {
+            web: 'http://natacioninfantil.com.ar/',
+            github: ''
+        }
+    },
+    {
+        ano: 2016,
+        titulo: 'Website para una maraton española',
+        lugar: '-',
+        stack: 'Wordpress',
+        links: {
+            web: 'http://lacursabadia.com/',
+            github: ''
+        }
+    },
+
+
+]
