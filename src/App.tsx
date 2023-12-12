294e3892fc65d24361b960f731deb756b4d73a26
Leandro Muzzupappa <leann.muzzu@gmail.com>
2023-12-12 14:53:19 -0300

    Add React base component and update global styles and favicon

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import './assets/styles/global.css';
+
+export const App = () => {
+  return (
+    <h2>Lennnnnnnnnnnnnnnny</h2>
+  )
+}
--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import "./assets/styles/style.css";
-import Pepitos from "./Pepitos/Pepitos";
-
-const canvas = document.createElement("canvas");
-canvas.classList.add("pepitos");
-document.body.appendChild(canvas);
-
-new Pepitos(canvas);
