decdd5aa82d8895843b914452d2df490e51f59e9
Leandro Muzzupappa <leann.muzzu@gmail.com>
2024-01-02 10:39:47 -0300

    Add Homepage component and styles

---
+++ src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,30 @@
+import { Headline } from "@components/Headline/Headline";
+import { Text } from "@components/Text/Text";
+import { Section } from "@layouts/Section/Section";
+import { Hero } from "@components/Hero/Hero";
+import styles from "./Homepage.module.css";
+
+export const Homepage = () => {
+  return (
+    <main className={`content-grid ${styles.homepage}`}>
+      <Hero />
+
+      <Section>
+        <Headline text="Experience." size="medium" />
+      </Section>
+      <Section>
+        <Headline text="Projects." size="medium" />
+      </Section>
+      <Section>
+        <Headline text="Forward" size="medium" />
+        <Headline text="-- Thinking." size="medium" />
+        <Text
+          text="Graphic Designer and self-taught Programmer weaving creativity and code to shape brands and websites."
+        />
+        <Text
+          text="I express myself through oil painting, mandala and lettering drawings, and from time to time pottery. I'm also coffee and tea enthusiast, so occasionally struggle with burning liquids. When not immersed in design and code, I love exploring Buenos Aires on my bike."
+        />
+      </Section>
+    </main>
+  );
+};
