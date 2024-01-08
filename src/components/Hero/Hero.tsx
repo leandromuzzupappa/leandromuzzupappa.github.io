e2150f0aa088df89f872708b5f3f2860de95b7d2
Leandro Muzzupappa <leann.muzzu@gmail.com>
2024-01-08 07:05:51 -0300

    Add z-index and background to Hero module CSS

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,8 +1,52 @@
+import { useRef, useState } from "react";
+import gsap from "gsap";
+import { useLenis } from "@studio-freight/react-lenis";
 import { Heading } from "@/components/Heading/Heading";
 import { Text } from "@components/Text/Text";
+import { ConicGradient } from "@components/heroBackgrounds/ConicGradient/ConicGradient";
 import styles from "./Hero.module.css";
 
 export const Hero = () => {
+  const bgRef = useRef<HTMLDivElement>(null);
+
+  const [scroll, setScroll] = useState(0);
+
+
+  useLenis((lenis) => {
+    const height = lenis.dimensions.height;
+
+    const borderRadius = Math.min(lenis.scroll / 12, 80);
+
+    if (lenis.scroll > height / 1.5) {
+      gsap.to(bgRef.current, {
+        borderRadius,
+        height: '50px',
+        maxWidth: '1360px',
+        scale: 1,
+        xPercent: -50,
+        y: 10,
+        duration: 1.2,
+        ease: "power4.out",
+      })
+    } else {
+      gsap.to(bgRef.current, {
+        borderRadius,
+        height: '100vh',
+        maxWidth: '100vw',
+        scaleX: Math.max(1 - lenis.scroll / 3000, .5),
+        scaleY: Math.max(1 - lenis.scroll / 900, .1),
+        xPercent: -50,
+        y: lenis.scroll * 0.01,
+        duration: 0.8,
+        ease: "power3.out",
+      });
+
+    }
+
+    setScroll(lenis.scroll);
+  })
+
+
   return (
     <section className={`full-width ${styles.hero}`}>
       <Heading
@@ -15,6 +59,9 @@ export const Hero = () => {
         classList={styles.copy}
         align="right"
       />
+      <div ref={bgRef} className={styles.bg}>
+        <ConicGradient scrollPercent={scroll} />
+      </div>
     </section>
   );
 };
