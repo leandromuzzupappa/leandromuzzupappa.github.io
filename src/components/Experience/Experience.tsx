e5037b473f424be31b489c1e0d5e76e9e7048c53
Leandro Muzzupappa <leann.muzzu@gmail.com>
2024-03-05 14:20:54 -0300

    Remove Accordion module CSS and Add Experience component

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,11 +1,11 @@
 import { AccordionItem } from "../AccordionItem/AccordionItem";
-import styles from './Accordion.module.css';
+import styles from './Experience.module.css';
 import { experienceData } from '@/data/experienceData';
 
 
-export const Accordion = () => {
+export const Experience = () => {
   return (
-    <div className={styles.accordion}>
+    <div className={styles.experience}>
       <ul>
         {experienceData.map((experience, index) => {
           return <AccordionItem key={experience.company + index} {...experience} />
